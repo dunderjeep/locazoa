4ed76d186f41705a1b60a831b4edf8388e079a56
Cliff Coulter <ccoulter@evenica.com>
2020-04-08 08:49:53 -0400

    updated build shell script format

--- continuous_deployment/build.sh
+++ continuous_deployment/build.sh
@@ -2,6 +2,7 @@
 
 set -ev
 #run only on master
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
-  npm run build
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]] 
+then
+   npm run build
 fi

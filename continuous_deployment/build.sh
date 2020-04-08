d4a03eea1d6eaeb517cf216bedea0f7b2028f74f
Cliff Coulter <ccoulter@evenica.com>
2020-04-08 08:57:39 -0400

    build test

--- continuous_deployment/build.sh
+++ continuous_deployment/build.sh
@@ -2,7 +2,7 @@
 
 set -ev
 #run only on master
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]] 
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]] || true
 then
    npm run build
 fi
